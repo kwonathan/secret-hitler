657259db07a44c4c0df8c0677e5d3ebec3906d69
John Kwon <teyunkwon@gmail.com>
2025-01-10 16:21:26 +0000

    Add legislative session

--- game.py
+++ game.py
@@ -4,13 +4,17 @@ class Game:
     def __init__(self, players):
         self.players = players
         self.policy_deck = random.shuffle(["Liberal"] * 6 + ["Fascist"] * 11)
+        if len(self.players) == 5 or len(self.players) == 6:
+            self.fascist_track = "5_6"
+        elif len(self.players) == 7 or len(self.players) == 8:
+            self.fascist_track = "7_8"
+        else:
+            self.fascist_track = "9_10"
 
         self.president = None
         self.chancellor = None
         self.president_index = 0
         self.election_tracker = 0
-        self.last_president = None
-        self.last_chancellor = None
         self.nominated_president = None
 
         self.liberal_policies = 0
@@ -36,8 +40,8 @@ class Game:
         current_game_state += f"{self.nominated_president.name} is selecting a Chancellor.\n"
         current_game_state += f"The election tracker is at {self.election_tracker}.\n"
         if self.game_state is not None:
-            current_game_state += f"The last President was {self.last_president}.\n"
-            current_game_state += f"The last Chancellor was {self.last_chancellor}.\n"
+            current_game_state += f"The last President was {self.president.name}.\n"
+            current_game_state += f"The last Chancellor was {self.chancellor.name}.\n"
         current_game_state += f"There are now {self.liberal_policies} Liberal Policies.\n"
         current_game_state += f"There are now {self.fascist_policies} Fascist Policies."
         self.game_state = current_game_state
@@ -77,9 +81,7 @@ class Game:
                 self.message_history.append(f"{player.name} votes {vote}")
             if votes.count("Ja!") > votes.count("Nein!"):
                 self.election_tracker = 0
-                self.last_president = self.president
                 self.president = self.nominated_president
-                self.last_chancellor = self.chancellor
                 self.chancellor = nominated_chancellor
                 self.message_history.append(f"Majority vote, {self.president.name} becomes the new President and {self.chancellor.name} becomes the new Chancellor.")
                 if self.fascist_policies >= 3:
@@ -91,7 +93,6 @@ class Game:
                 self.election_tracker += 1
                 if self.election_tracker == 3:
                     top_policy = self.draw_pile.pop(0)
-                    self.discard_pile.append(top_policy)
                     self.message_history.append(f"Not a majority vote, and the election tracker has moved by 1 to 3. The top Policy in the Policy deck will now be enacted, which is a {top_policy} Policy.")
                     self.election_tracker = 0
                     if top_policy == "Liberal":
@@ -117,6 +118,41 @@ class Game:
             return
 
         # LEGISLATIVE SESSION
+        policy_candidates = self.draw_pile[:3]
+        self.draw_pile = self.draw_pile[3:]
+        discarded_policy, policy_candidates = self.president.enact_policy(policy_candidates)
+        self.discard_pile.append(discarded_policy)
+        discarded_policy, enacted_policy = self.chancellor.enact_policy(policy_candidates)
+        self.discard_pile.append(discarded_policy)
+
+        if enacted_policy == "Liberal":
+            self.liberal_policies += 1
+            if self.liberal_policies == 5:
+                self.five_liberal_policies_enacted = True
+        else:
+            self.fascist_policies += 1
+            if self.fascist_policies == 6:
+                self.six_fascist_policies_enacted = True
+
+        if len(self.draw_pile) < 3:
+            new_policy_deck = random.shuffle(self.draw_pile + self.discard_pile)
+            self.draw_pile = new_policy_deck
+            self.discard_pile = []
+
+        self.message_history.append(f"The President and Chancellor have enacted a {enacted_policy} Policy.")
+
+        # Check win condition
+        if self.five_liberal_policies_enacted or self.six_fascist_policies_enacted:
+            return
+
+        if enacted_policy == "Liberal":
+            return
+        if enacted_policy == "Fascist" and self.fascist_track == "5_6" and self.fascist_policies <= 2:
+            return
+        if enacted_policy == "Fascist" and self.fascist_track == "7_8" and self.fascist_policies == 1:
+            return
+
+        # EXECUTIVE ACTION
         # ...
 
     def check_win_condition(self):
